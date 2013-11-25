68c1ded806e7d70ea61ce33ceed7f0e134d8193f
Karthik Mallavarapu <karthik.mallavarapu@gmail.com>
2013-11-25 21:09:43 +0200

    added projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,21 @@ class ProjectsController < ApplicationController
 	def new
 		@project = Project.new
 	end
+
+	def create
+		@project = Project.new(project_params)
+		@project.save
+		flash[:notice] = "Project has been created"	
+		redirect_to @project
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	private
+# With rails 4, every attribute modification must be explicitly permitted.
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
--- features/step_definitions/step_definitions.rb
+++ features/step_definitions/step_definitions.rb
@@ -3,17 +3,17 @@ Given(/^I am on the homepage$/) do
 end
 
 When(/^I follow "(.*?)"$/) do |arg1|
-	click_on(arg1)
+	click_on arg1
 end
 
 When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
-fill_in(arg2, with: arg1)
+	fill_in(arg1, with: arg2)
 end
 
 When(/^I press "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+	click_on arg1
 end
 
 Then(/^I should see "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+	has_content?(arg1)
 end
